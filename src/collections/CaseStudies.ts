e41ce71e6854f3bc23fe437b470b22a10a3bee9b
m.darmiche <m.darmiche@coozzy.ch>
2023-02-08 15:26:26 +0100

    change uses

--- src/collections/CaseStudies.ts
+++ src/collections/CaseStudies.ts
@@ -1,26 +1,8 @@
 import { CollectionConfig } from 'payload/types';
 import { isAdmin } from '../access/isAdmin';
 import { publishedOnly } from '../access/publishedOnly';
-import richText from '../fields/richText';
-import { CallToAction } from '../blocks/CallToAction';
-import { CardGrid } from '../blocks/CardGrid';
-import { CaseStudiesHighlight } from '../blocks/CaseStudiesHighlight';
-import { Content } from '../blocks/Content';
-import { ContentGrid } from '../blocks/ContentGrid';
-import { Form } from '../blocks/Form';
-import { LinkGrid } from '../blocks/LinkGrid';
-import { MediaBlock } from '../blocks/Media';
-import { MediaContent } from '../blocks/MediaContent';
-import { ReusableContent } from '../blocks/ReusableContent';
-import { Slider } from '../blocks/Slider';
-import { Steps } from '../blocks/Steps';
-import { CodeFeature } from '../blocks/CodeFeature';
-import { HoverHighlights } from '../blocks/HoverHighlights';
-import { StickyHighlights } from '../blocks/StickyHighlights';
-import { slugField } from '../fields/slug';
 import { formatPreviewURL } from '../utilities/formatPreviewURL';
 import { regeneratePage } from '../utilities/regeneratePage';
-import largeBody from '../fields/richText/largeBody';
 
 export const CaseStudies: CollectionConfig = {
   slug: 'case-studies',
@@ -41,62 +23,74 @@ export const CaseStudies: CollectionConfig = {
   hooks: {
     afterChange: [
       ({ req: { payload }, doc }) => {
-        regeneratePage({
+       /* regeneratePage({
           payload,
           collection: 'case-studies',
           doc
-        });
+        });*/
       },
     ]
   },
   fields: [
     {
+      name: 'position',
+      type: 'number',
+      required: true,
+    },
+    {
+      name: 'rating',
+      type: 'number',
+      required: false,
+    },
+    {
+      name: 'reviews',
+      type: 'number',
+      required: false,
+    },
+    {
       name: 'title',
       type: 'text',
-      required: true,
-    },
-    richText({
-      name: 'introContent',
-      admin: {
-        elements: ['h1', largeBody],
-        leaves: ['underline'],
-      }
-    }),
-    {
-      name: 'featuredImage',
-      type: 'upload',
-      relationTo: 'media',
-      required: true,
+      required: false,
     },
     {
-      name: 'layout',
-      type: 'blocks',
-      blocks: [
-        CallToAction,
-        CardGrid,
-        CaseStudiesHighlight,
-        CodeFeature,
-        Content,
-        ContentGrid,
-        Form,
-        HoverHighlights,
-        LinkGrid,
-        MediaBlock,
-        MediaContent,
-        ReusableContent,
-        Slider,
-        Steps,
-        StickyHighlights,
-      ]
-    },
-    slugField(),
-    {
-      name: 'url',
-      label: 'URL',
+      name: 'photos_link',
       type: 'text',
-      admin: {
-        position: 'sidebar'
-      }
+      required: false,
+    },
+    {
+      name: 'address',
+      type: 'text',
+      required: false,
+    },
+    {
+      name: 'phone',
+      type: 'text',
+      required: false,
+    },
+    {
+      name: 'website',
+      type: 'text',
+      required: false,
+    },
+    {
+      name: 'description',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'thumbnail',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'latitude',
+      type: 'number',
+      required: false,
+    },
+    {
+      name: 'longitude',
+      type: 'number',
+      required: false,
     }
   ]
 }
