579f39755126a5ce3f3b2cfe3a577780b6af0666
m.darmiche <m.darmiche@coozzy.ch>
2023-02-09 10:10:38 +0100

    Fix

--- src/utilities/getSerpapiData.ts
+++ src/utilities/getSerpapiData.ts
@@ -1,7 +1,7 @@
 import { Payload } from "payload";
 // import { formatPagePath } from "./formatPagePath";
-import type { GoogleMapsParameters } from 'serpapi';
-import { getJson } from 'serpapi';
+// import type { GoogleMapsParameters } from 'serpapi';
+// import { getJson } from 'serpapi';
 
 export const getSerpapiData = async ({
                                          doc,
@@ -14,15 +14,17 @@ export const getSerpapiData = async ({
 }): Promise<void> => {
 
     try {
-        const params = {
+        /*const params = {
             q: "coffee",
             ll: "@40.7455096,-74.0083012,15.1z",
             type: "search",
             api_key: "ba9a3e5451c9d663d8a75d8e5ca834a8fb68fa89722aa1c0655183aa833eb321"
         } as GoogleMapsParameters;
         // Show result as JSON
-        const response = await getJson("google_maps", params);
-        payload.logger.info(`Revalidated path coffee ${response["local_results"]}`);
+        const response = await getJson("google_maps", params);*/
+        // https://serpapi.com/search.json?engine=google_maps&q=coffee&ll=@40.7455096,-74.0083012,15.1z&type=search
+        const res = await fetch(`https://serpapi.com/search.json?engine=google_maps&q=coffee&ll=@40.7455096,-74.0083012,15.1z&type=search`);
+        payload.logger.info(`Revalidated path coffee ${res["local_results"]}`);
     }catch (err) {
         payload.logger.error(`Error hitting revalidate route for coffee}`);
     }
