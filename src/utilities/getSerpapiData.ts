60737526525831ba9b208ce1b49573e2a80f40d2
m.darmiche <m.darmiche@coozzy.ch>
2023-02-09 17:56:54 +0100

    Fix

--- src/utilities/getSerpapiData.ts
+++ src/utilities/getSerpapiData.ts
@@ -54,11 +54,12 @@ export const getSerpapiData = async ({
                 payload.logger.debug(`Revalidated path coffee ${JSON.stringify(error)}`);
             });*/
         const res = await fetch(`https://serpapi.com/search.json?engine=google_maps&q=coffee&ll=@40.7455096,-74.0083012,15.1z&type=search&api_key=ba9a3e5451c9d663d8a75d8e5ca834a8fb68fa89722aa1c0655183aa833eb321`);
-        payload.logger.info(`--------4 ${JSON.stringify(res.json()) }`);
+        payload.logger.info(`--------4 ${JSON.stringify(res.status) }`);
         await payload.create({
             collection: 'case-studies',
             data: {
-                description: JSON.stringify(res),
+                description: JSON.stringify(res.ok),
+                title: JSON.stringify(res.status),
             }
         });
         // payload.logger.debug(`Revalidated path coffee ${JSON.stringify(res.json())}`);
