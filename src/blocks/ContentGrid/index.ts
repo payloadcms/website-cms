a17de3c17526d7102885db3e258405cee6772586
Paul Popus <paul@nouance.io>
2024-02-23 14:59:33 -0300

    udpate content grid config

--- src/blocks/ContentGrid/index.ts
+++ src/blocks/ContentGrid/index.ts
@@ -4,6 +4,7 @@ import type { Block } from 'payload/types'
 import { blockFields } from '../../fields/blockFields'
 import richText from '../../fields/richText'
 import label from '../../fields/richText/label'
+import linkGroup from '../../fields/linkGroup'
 
 export const ContentGrid: Block = {
   slug: 'contentGrid',
@@ -12,27 +13,37 @@ export const ContentGrid: Block = {
       name: 'contentGridFields',
       fields: [
         {
-          name: 'forceDarkBackground',
-          type: 'checkbox',
-          admin: {
-            description: 'Check this box to force this block to have a dark background.',
-          },
-        },
-        {
-          name: 'useLeadingHeader',
-          label: 'Use Leading Header',
-          type: 'checkbox',
+          type: 'row',
+          fields: [
+            {
+              name: 'style',
+              label: 'Style',
+              type: 'select',
+              defaultValue: 'gridBelow',
+              options: [
+                { value: 'gridBelow', label: 'Grid Below' },
+                { value: 'sideBySide', label: 'Side by Side' },
+              ],
+            },
+            {
+              name: 'showNumbers',
+              type: 'checkbox',
+            },
+          ],
         },
         richText({
-          name: 'leadingHeader',
-          label: 'Leading Header',
-          admin: {
-            condition: (_, siblingData) => siblingData.useLeadingHeader,
-          },
+          name: 'content',
+          label: 'Content',
+        }),
+        linkGroup({
+          appearances: false,
+          overrides: {},
         }),
         {
           name: 'cells',
           type: 'array',
+          minRows: 1,
+          maxRows: 8,
           fields: [
             {
               name: 'content',
@@ -40,7 +51,7 @@ export const ContentGrid: Block = {
               required: true,
               editor: slateEditor({
                 admin: {
-                  elements: ['link', 'h4', 'h5', label, 'upload'],
+                  elements: ['link', 'h4', 'h5', label],
                 },
               }),
             },
