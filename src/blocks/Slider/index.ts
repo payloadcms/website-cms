b60551f7b8568c3540431318cf19f458938ba3d7
Paul <paul@payloadcms.com>
2024-03-01 17:43:16 -0300

    feat: update slider block (#75)
    * commit
    
    * update config slider block
    
    * remove slider type
    
    * remove excess slide options

--- src/blocks/Slider/index.ts
+++ src/blocks/Slider/index.ts
@@ -10,69 +10,33 @@ export const Slider: Block = {
       name: 'sliderFields',
       fields: [
         {
-          name: 'useLeadingHeader',
-          label: 'Use Leading Header',
-          type: 'checkbox',
-        },
-        richText({
-          name: 'leadingHeader',
-          label: 'Leading Header',
-          admin: {
-            condition: (_, siblingData) => siblingData.useLeadingHeader,
-          },
-        }),
-        {
-          type: 'select',
-          name: 'sliderType',
-          required: true,
-          options: [
-            {
-              label: 'Quote Slider',
-              value: 'quoteSlider',
-            },
-            {
-              label: 'Image Slider',
-              value: 'imageSlider',
-            },
-          ],
-        },
-        {
-          type: 'array',
-          name: 'imageSlides',
-          required: true,
-          minRows: 3,
-          admin: {
-            condition: (_, siblingData) => siblingData.sliderType === 'imageSlider',
-          },
-          fields: [
-            {
-              type: 'upload',
-              name: 'image',
-              relationTo: 'media',
-              required: true,
-            },
-          ],
-        },
-        {
           type: 'array',
           name: 'quoteSlides',
           required: true,
           minRows: 3,
-          admin: {
-            condition: (_, siblingData) => siblingData.sliderType === 'quoteSlider',
-          },
           fields: [
-            richText({
-              name: 'richText',
-              required: true,
-            }, {
-              elements: [],
-              leaves: ['underline'],
-            }),
             {
-              type: 'date',
-              name: 'quoteDate',
+              type: 'text',
+              name: 'leader',
+            },
+            {
+              type: 'textarea',
+              name: 'quote',
               required: true,
+            },
+            {
+              type: 'row',
+              fields: [
+                {
+                  type: 'text',
+                  name: 'author',
+                  required: true,
+                },
+                {
+                  type: 'text',
+                  name: 'role',
+                },
+              ],
             },
           ],
         },
